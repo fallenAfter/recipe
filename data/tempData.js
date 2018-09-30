b4bb6b9be306d138dffdc31e5d4a9f175bbf25b4
Brandon Elliott <belliott@keshetinc.com>
2018-09-30 12:09:38 -0400

    Add Ui Elements. set some colors

--- data/tempData.js
+++ data/tempData.js
@@ -1,5 +1,12 @@
 module.exports = {
-	recipeTitle: "PB and J",
-	ingredients: [{name: "Peanut Butter", ammount: 2, measurment: "tbsp"}, {name: "Jelly", ammount: 2, measurment: "tbsp"}, {name: "bread", ammount: 2, measurment: "slices"}],
-	steps: [{number: 1, instructions: "Evenly spread peanutbutter to one side of the first slice of bread"}, {number: 2, instruction: "Evenly spread jelly on second peice of bread"}, {number:3, instruction: "place the two peices of bread peanut butter and jelly side together"}]
+	title: "PB and J",
+	image: "sandwich.png",
+	ingredients: [{name: "Peanut Butter", ammount: 3, measurment: "tbsp"}, {name: "Jelly", ammount: 2, measurment: "tbsp"}, {name: "bread", ammount: 2, measurment: "slices"}],
+	steps: [{number: 1, instruction: "Evenly spread peanutbutter to one side of the first slice of bread"},
+	{number: 2, instruction: "Evenly spread jelly on second peice of bread"},
+	{number:3, instruction: "place the two peices of bread peanut butter and jelly side together"},
+	{number:4, instruction: "redundant step 1"},
+	{number:5, instruction: "redundant step 2"},
+	{number:6, instruction: "redundant step 3"},
+	{number:7, instruction: "redundant step 4"}]
 }
--- public/core.js
+++ public/core.js
@@ -1,14 +1,100 @@
+'use strict';
 var recipe = angular.module('recipe', []);
+//angular.controller('mainController', ['$scope', '$http', '$q'], mainController($scope, $http, $q))
 
-function mainController($scope, $http){
-	$scope.formData = {};
+/*app.service('Recipe', [$http], function($http, $q){
 
-	$http.get('/api/recipe')
-		.success(function(data){
-			$scope.recipe = data;
-			console.log(data);
-		})
-		.error(function(data){
-			console.log('Error: ' + data);
+})*/
+
+
+function mainController($scope, $http, $q){
+	main();
+
+	//console.log($q);
+
+	async function main(){
+		var deferred = $q.defer();
+		$scope.formData = {};
+		$scope.recipe = null;
+		recipe = null
+		var recipe = null;
+		
+		await getDataAsync('/api/recipe').then(c => recipe = c);
+		
+		var steps = createStepTiles(recipe.steps, recipe.image);
+		console.log(steps);
+		$scope.$apply(function(){
+			$scope.recipe = recipe;
+			$scope.steps = steps;
+			$scope.getTileInclude = getTileInclude;
 		});
+		
+	}
+
+	function createStepTiles(steps, image){
+		var stepTiles = [];
+		//check if there is an image
+		if(image != undefined && image != null){
+			stepTiles.push({
+				empty: false,
+				image: true,
+				imageUrl: image,
+				number: 0,
+				instructions: ""
+			});
+		}
+		//create tile object for each step
+		for(var cStep = 0; cStep < steps.length; cStep++){
+			var step = steps[cStep];
+			stepTiles.push({
+				empty: false,
+				image: false,
+				imageUrl:"",
+				number: step.number,
+				instructions: step.instruction
+			});
+		}
+
+		if(stepTiles.length%6 != 0){
+			for(var cExtra = 0; cExtra < stepTiles.length%6; cExtra++){
+				stepTiles.push({
+					empty: true,
+					image: false,
+					imageUrl:"",
+					number: "",
+					instructions: ""
+				});
+			}
+		}
+
+		return {
+			tileCount: stepTiles.length,
+			tiles: stepTiles
+		};
+	}
+
+	function getDataAsync(endpoint){
+		var deferred = $q.defer();
+
+		$http.get(endpoint)
+				.then(function(res){
+					deferred.resolve(res.data);
+				});
+		return deferred.promise;
+	}
+	
+
+	function getTileInclude(step){
+		var prefix = "templates/";
+		if(step.image == true){
+			return prefix+"tileImage.html";
+		}
+		else if(step.empty == false){
+			return prefix+"tileStep.html";
+		}
+		else if(step.empty == true){
+			return prefix+"tileEmpty.html";
+		}
+
+	}
 }
